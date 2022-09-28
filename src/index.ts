57a45a5091b76339d6da636ddcba1e04547f7072
AntonGreg <antonio.pena@epn.edu.ec>
2022-09-28 10:34:23 -0500

    fix: cors blocked

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
-import express from "express";
-import diagnoseRouter from "./routes/diagnoses";
-import patientRoute from "./routes/patients";
+import express from 'express';
+import diagnoseRouter from './routes/diagnoses';
+import patientRoute from './routes/patients';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 
 const PORT = 3001;
 
-app.get("/api/ping", (_req, res) => {
-  console.log("someone pinged here");
-  res.send("pong");
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.get('/api/ping', (_req, res) => {
+  console.log('someone pinged here');
+  res.send('pong');
 });
 
-app.use("/api/diagnoses", diagnoseRouter);
-app.use("/api/patients", patientRoute);
+app.use('/api/diagnoses', diagnoseRouter);
+app.use('/api/patients', patientRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
